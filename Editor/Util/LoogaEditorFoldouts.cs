6a8693b0c84e6b98eee33133d3d82cf312f410a9
Ben Holicek <bh4786@gmail.com>
2026-06-23 11:03:04 -0500

    triangle resize

--- Editor/Util/LoogaEditorFoldouts.cs
+++ Editor/Util/LoogaEditorFoldouts.cs
@@ -627,9 +627,9 @@ namespace LoogaSoft.Inspector.Editor
             Vector3[] points = expanded
                 ? new[]
                 {
-                    new Vector3(center.x - radius, center.y - verticalRadius, 0f),
-                    new Vector3(center.x + radius, center.y - verticalRadius, 0f),
-                    new Vector3(center.x, center.y + verticalRadius, 0f)
+                    new Vector3(center.x - radius, center.y - verticalRadius * 0.75f, 0f),
+                    new Vector3(center.x + radius, center.y - verticalRadius * 0.75f, 0f),
+                    new Vector3(center.x, center.y + verticalRadius * 0.75f, 0f)
                 }
                 : new[]
                 {
